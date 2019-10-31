745a1cb28940315da37ed6891e5d4df83ac79ac7
Keith Jones <kjsothermail+github@gmail.com>
2019-10-31 16:10:45 -0500

    First commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private int OldMouseXPos;
+    private int OldMouseYPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementSpeed = 16f;
+        float dragMovementSpeed = 1.5f;
+        float scrollSpeed = 380f;
+
+        Vector3 newCameraPos = this.transform.position;
+
+        if (Input.GetMouseButton(0))
+        {
+            float diffX, diffY;
+            Vector3 diffVector = new Vector3();
+
+            diffX = this.OldMouseXPos - Input.mousePosition.x;
+            diffY = this.OldMouseYPos - Input.mousePosition.y;
+
+            diffVector.Set(diffX, 0, diffY);
+
+            diffVector = diffVector * dragMovementSpeed * Time.deltaTime;
+            newCameraPos += diffVector;
+
+        } else
+        {
+            
+            
+
+            if(Input.GetKey("w"))
+            {
+                newCameraPos.z += movementSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey("s"))
+            {
+                newCameraPos.z -= movementSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey("a"))
+            {
+                newCameraPos.x -= movementSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey("d"))
+            {
+                newCameraPos.x += movementSpeed * Time.deltaTime;
+            }
+
+        }
+
+        
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        
+        newCameraPos.y -= scroll * Time.deltaTime * scrollSpeed;
+        if (newCameraPos.y > 2.5) newCameraPos.z += scroll * Time.deltaTime * scrollSpeed;
+        if (newCameraPos.y < 2.5) newCameraPos.y = 2.5f;
+        
+
+        this.transform.position = newCameraPos;
+
+        this.OldMouseXPos = (int)Input.mousePosition.x;
+        this.OldMouseYPos = (int)Input.mousePosition.y;
+
+
+        RaycastHit hit;
+        Camera camera = GetComponent<Camera>();
+        Debug.Log(camera.name);
+        Ray cameraRay = camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+
+        if (Physics.Raycast(cameraRay, out hit)) {
+
+            Transform objectHit = hit.transform;
+
+            Debug.Log("I hit " + objectHit.name);
+        
+        }       
+
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/MakeGrid.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+public class MakeGrid : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameObject baseHexagon = GameObject.Instantiate(GameObject.Find("baseHexagon"));
+        Quaternion baseQuaternion = baseHexagon.transform.rotation;
+
+        int width = 60;
+        int height = 60;
+
+        GameObject[][] tiles = new GameObject[width][];
+
+        double xOffset = 2.26f;
+        double zOffset = 1.16f;
+
+        for (int col = 0; col < width; col++)
+        {
+
+            tiles[col] = new GameObject[height];
+
+            for (int row = 0; row < height; row++)
+            {
+
+                tiles[col][row] = GameObject.Instantiate(baseHexagon);
+
+                Vector3 nextPos = new Vector3();
+                float oddOffset = col % 2 > 0 ? (float)zOffset : 0;
+
+                nextPos.Set((float)xOffset * col, 0, oddOffset + ((float)zOffset * row * 2));
+                tiles[col][row].transform.SetPositionAndRotation(nextPos, baseQuaternion);
+                tiles[col][row].name = row.ToString() + ", " + col.ToString();
+
+                Renderer rend = null;
+                rend = tiles[col][row].GetComponent<Renderer>();
+                rend.enabled = true;
+
+                if (row % 2 != 0)
+                {
+                    Color oddCol = new Color();
+                    oddCol.r = 1;
+                    
+                }
+
+            }
+        }
+
+        Vector3 lineOffset = new Vector3();
+        lineOffset.Set(0.0f, 2.0f, 0.0f);
+
+        GameObject line = new GameObject("lineObject");
+
+        LineRenderer lineTest = line.AddComponent<LineRenderer>();
+        lineTest.startWidth = 2.0f;
+        lineTest.endWidth = 2.0f;
+        lineTest.material = new Material(Shader.Find("Standard"));
+        lineTest.SetColors(Color.red, Color.blue);
+        lineTest.SetPosition(0, tiles[20][20].transform.position + lineOffset);
+        lineTest.SetPosition(1, tiles[24][24].transform.position + lineOffset);
+
+        GameObject tmp = GameObject.Find("Tribeling");
+        Vector3 Tribeling_Location = tiles[10][30].transform.position;
+        tmp.gameObject.transform.position = Tribeling_Location;
+
+        GameObject MainCamera = GameObject.Find("Geo Camera");
+        Vector3 CameraOffset = new Vector3(0f, 6f, -30f);
+        Vector3 Camera_Location = tiles[30][30].transform.position ;
+        MainCamera.gameObject.transform.position = Camera_Location + CameraOffset;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
